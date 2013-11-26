d8ec02696e6d05443d5146d04067a3f16df8582b
lmoux <luismouxdominguez@gmail.com>
2013-11-26 09:59:03 -0800

    Decided I want to be more ambitious with my practice
    - I want to practice some common algorithms
    - I want to practice some common data strucutures
    - I want to practice some F#
    
    To that extent, I have now included a beefier domain model to characterize problems. It is written in F#. I need to find out how to partially apply String.concat but it is mostly done.

---
+++ Puzzles/Puzzels.DomainModel/Puzzle.fs
@@ -0,0 +1,73 @@
+﻿namespace Puzzles.DomainModel
+
+type Utils =
+    static member CombineText x y = x + System.Environment.NewLine + y
+    // LMD> Somehow need to curry/partially apply String.concat with my seeded spearator = NewLine
+    ////static member PrettyPrintDescription string seq = List.reduce Utils.CombineText 
+
+type BigOh(expression : string) =
+    member this.Expression = expression
+
+module Algorithms =
+    [<RequireQualifiedAccess>]
+    type Description =
+    | Sorting of isStable:bool * averageCase:BigOh * worstCase:BigOh
+    | Hashing
+    | Compression
+    
+    [<RequireQualifiedAccess>]
+    type Algorithm(name:string, description:Description) =
+        member this.Name = name
+        member this.Title = sprintf "Algorithm: %s" this.Name
+        member this.Description = description
+        override this.ToString() =
+            Utils.CombineText this.Title (this.Description.ToString())
+
+module DataStructures =
+    [<RequireQualifiedAccess>]
+    type Description =
+    | Linear
+    | Trees
+    | Hashes
+    | Graphs
+
+    [<RequireQualifiedAccess>]
+    type DataStructure(name:string, description:Description) =
+        member this.Name = name
+        member this.Title = sprintf "Data structure: %s" this.Name
+        member this.Description = description
+        override this.ToString() =
+            Utils.CombineText this.Title (this.Description.ToString())
+
+module ProjectEuler =
+
+    [<RequireQualifiedAccess>]
+    type Description =
+    | Simple of string
+    | WithAlgorithms of Description * algorithmsInvolved:Algorithms.Algorithm seq
+    | WithDataStructures of Description * dataStructuresInvolved:DataStructures.DataStructure seq
+
+    [<RequireQualifiedAccess>]
+    type Problem(id:int, description:Description) =
+        member this.Id = id
+        member this.Title = sprintf "Project Euler #%d" id
+        member this.Description = description
+        override this.ToString() =
+            Utils.CombineText this.Title (this.Description.ToString())
+
+module Puzzle =
+    [<RequireQualifiedAccess>]
+    type Identifier =
+        | GeneralMathematical of name:string * description:string
+        | GeneralLogic of name:string * description:string
+        | ProjectEuler of ProjectEuler.Problem
+        | DataStructure of DataStructures.DataStructure
+        | Algorithm of Algorithms.Algorithm
+        override this.ToString() =
+            match this with
+                | GeneralMathematical(name, description) -> sprintf "General math problem: %s" name
+                | GeneralLogic(name, description) -> sprintf "General logic problem: %s" name
+                | ProjectEuler(proper) -> proper.ToString()
+                | DataStructure(proper) -> proper.ToString()
+                | Algorithm(proper) -> proper.ToString()
+
